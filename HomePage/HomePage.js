e7219e004316a98d8b784cc85c4145e63d713718
Douglas Cristiano <164531720+DougCristiano@users.noreply.github.com>
2025-02-10 18:43:56 -0300

    Home Page correta

--- HomePage/HomePage.js
+++ HomePage/HomePage.js
@@ -80,6 +80,56 @@ function listadelobos() {
 
   let lobo1 = document.querySelector(".lobo_exemplo1");
   lobo1.append(engloba);
+
+  let engloba2 = document.createElement("div");
+
+  let imagem_exemplo2 = document.createElement("div");
+
+  let fundo_azul2 = document.createElement("div");
+
+  let link_foto2 = document.createElement("a");
+    link_foto2.href = "../ShowLobo/ShowLobo.html";
+    link_foto2.addEventListener("click", function () {
+      localStorage.setItem("lobotemp", segundolobo.id);
+      window.location.href = "ShowLobo.html";
+  });
+
+  let foto2 = document.createElement("img");
+    foto2.src = "../images/loboexemplo2.png";
+    foto2.alt = "Imagem do Lobo";
+
+  let texto_exemplo2 = document.createElement("div");
+
+  let nome2 = document.createElement("h2");
+  nome2.innerText = segundolobo.nome;
+
+  let idade2 = document.createElement("p");
+  idade2.innerText = "Idade: " + segundolobo.idade;
+
+  let descricao2 = document.createElement("p");
+  descricao2.innerText = segundolobo.descricao;
+
+  engloba2.classList.add("engloba2");
+  imagem_exemplo2.classList.add("imagem_exemplo2");
+  fundo_azul2.classList.add("fundo_azul2");
+  foto2.classList.add("imagem2");
+  texto_exemplo2.classList.add("texto_exemplo2");
+  nome2.classList.add("nome2");
+  idade2.classList.add("idade2");
+  descricao2.classList.add("descricao2");
+
+  link_foto2.append(foto2);
+  imagem_exemplo2.append(fundo_azul2);
+  imagem_exemplo2.append(link_foto2);
+  texto_exemplo2.append(nome2);
+  texto_exemplo2.append(idade2);
+  texto_exemplo2.append(descricao2);
+
+  engloba2.append(imagem_exemplo2);
+  engloba2.append(texto_exemplo2);
+
+  let lobo2 = document.querySelector(".lobo_exemplo2")
+  lobo2.append(engloba2)
 }
 
 document.addEventListener("DOMContentLoaded", listadelobos());
